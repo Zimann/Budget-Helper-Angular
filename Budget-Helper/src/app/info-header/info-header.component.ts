ea1b6e28641b907c4b22edf5159ee9b86820e79d
ziman <ziman_nicu@yahoo.com>
2019-06-23 19:53:08 +0300

    Add fonts & create dynamic date display

--- Budget-Helper/src/app/info-header/info-header.component.ts
+++ Budget-Helper/src/app/info-header/info-header.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info-header.component.scss']
 })
 export class InfoHeaderComponent implements OnInit {
-
+  currentMonth: string;
+  monthArray = [
+    'January',
+    'February',
+    'March',
+    'April',
+    'May',
+    'June',
+    'July',
+    'August',
+    'September',
+    'October',
+    'November',
+    'December'
+  ];
   constructor() { }
 
   ngOnInit() {
+    this.processCurrentDate();
+  }
+
+  processCurrentDate() {
+    this.currentMonth = this.monthArray[new Date().getMonth()];
   }
 
 }
