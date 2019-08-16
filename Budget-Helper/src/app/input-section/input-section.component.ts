60b2696230f6956111ae21ff4c52911ed21d8573
ziman <ziman_nicu@yahoo.com>
2019-08-17 00:30:10 +0300

    show total accumulated value & show error message on empty inputs

--- Budget-Helper/src/app/input-section/input-section.component.ts
+++ Budget-Helper/src/app/input-section/input-section.component.ts
@@ -1,4 +1,4 @@
-import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {AfterViewInit, ChangeDetectorRef, Component, OnInit} from '@angular/core';
 import {ViewChild, ElementRef} from '@angular/core';
 import {OperationService} from '../operation.service';
 
@@ -7,16 +7,17 @@ import {OperationService} from '../operation.service';
   templateUrl: './input-section.component.html',
   styleUrls: ['./input-section.component.scss']
 })
-export class InputSectionComponent implements OnInit {
+export class InputSectionComponent implements OnInit, AfterViewInit {
 
   private operatorSign: string;
-  public totalIncome: number;
+  public showErrorMessage = false;
   @ViewChild('inputDescription', {static: false}) inputDescription: ElementRef;
   @ViewChild('inputNumber', {static: false}) inputNumber: ElementRef;
   constructor(private operationService: OperationService) { }
 
   // handle the operation sign (+ or -)
   getSelectOption(data) {
+    this.showErrorMessage = false;
     if (data.target.value === '+') {
       this.operatorSign = '+';
       return this.operatorSign;
@@ -26,11 +27,24 @@ export class InputSectionComponent implements OnInit {
   }
 
   // grab the field values (added number and description)
- public pushData() {
+ public pushData(key = null) {
+    console.log(key);
+    // check if both the text description and value input fields have values in them
+    if (this.inputDescription.nativeElement.value !== '' && this.inputNumber.nativeElement.value !== '') {
       this.operationService.processFigures(Number(this.inputNumber.nativeElement.value), this.operatorSign);
+      this.inputDescription.nativeElement.value = '';
+      this.inputNumber.nativeElement.value = '';
+      if (this.showErrorMessage) {
+        this.showErrorMessage = !this.showErrorMessage;
+      }
+    } else { this.showErrorMessage = true; }
+
   }
 
-  ngOnInit() {
+  ngOnInit(): void {
   }
 
+  ngAfterViewInit(): void {
+
+  }
 }
