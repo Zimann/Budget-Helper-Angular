286ee7351c88b898bcec0b1af9e174bacf0e58f4
Nico Ziman <nziman@ausy-group.com>
2019-08-20 14:10:40 +0300

    create expense item to render on the 'Expenses' overview

---
+++ Budget-Helper/src/app/expense-item/expense-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-expense-item',
+  templateUrl: './expense-item.component.html',
+  styleUrls: ['./expense-item.component.scss']
+})
+export class ExpenseItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
