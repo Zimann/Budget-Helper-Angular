c04ee97412e5f1e786e89655b39bb0636a5549d8
Nico Ziman <nziman@ausy-group.com>
2019-08-20 14:15:47 +0300

    move template from main-overview into separate components

--- Budget-Helper/src/app/main-overview/expense-overview/expense-overview.component.ts
+++ Budget-Helper/src/app/main-overview/expense-overview/expense-overview.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {OperationService} from '../../operation.service';
 
 @Component({
   selector: 'app-expense-overview',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExpenseOverviewComponent implements OnInit {
 
-  constructor() { }
+  public addedEntries = this.operationService.expenseEntries;
+
+  constructor(private operationService: OperationService) { }
 
   ngOnInit() {
   }
